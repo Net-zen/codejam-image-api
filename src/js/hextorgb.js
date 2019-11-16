ca1825ee4e1a6afc53f4296d779d7ff59b3ff688
Net-zen <prankster@myttk.ru>
2019-11-16 19:56:53 +0600

    feat: pure dist

--- src/js/hextorgb.js
+++
@@ -1,8 +0,0 @@
-export default function hexToRGB(hex) {
-  const newHex = hex.replace('#', '');
-  const r = parseInt(newHex.substring(0, 2), 16);
-  const g = parseInt(newHex.substring(2, 4), 16);
-  const b = parseInt(newHex.substring(4, 6), 16);
-
-  return `rgb(${r}, ${g}, ${b})`;
-}
